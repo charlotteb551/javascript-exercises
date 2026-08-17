6e7f3c591c9b6ab0f85e8f9a6bdacc3671212c3a
charlotteb551 <295230572+charlotteb551@users.noreply.github.com>
2026-08-17 19:56:49 +0100

    Task 09 Complete

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    if (num1 > 0 && Number.isInteger(num1) && num2 > 0 && Number.isInteger(num2)){
+        sum=0
+        if (num2>num1){
+           for(i=num1;i<num2+1;i++){
+                sum+=i;
+            } 
+        }else{
+            for(i=num2;i<num1+1;i++){
+                sum+=i
+            }
+        }
+        return sum
+    }else{
+        return "ERROR"
+    }
 
 };
 

5dfb0da3f9f2a114711a416a2f2c6173ad8049f3
charlotteb551 <295230572+charlotteb551@users.noreply.github.com>
2026-08-17 14:20:55 +0100

    Task 07 Complete

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverseString="";
+    let stringLength=string.length;
+    for (let i=0;i<stringLength;i++){
+        reverseString+=string[stringLength-1-i];
+    }
+    return reverseString;
 };
 
 // Do not edit below this line

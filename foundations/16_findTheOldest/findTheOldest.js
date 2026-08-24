5d31f091d6ad1075194405cf858c2351cb817614
charlotteb551 <295230572+charlotteb551@users.noreply.github.com>
2026-08-24 12:04:42 +0100

    Task 16 Complete

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentYear = new Date().getFullYear();
 
+    return array.reduce((oldest, person) => {
+        const deathYear = person.yearOfDeath || currentYear;
+        const oldestDeathYear = oldest.yearOfDeath || currentYear;
+
+        const age = deathYear - person.yearOfBirth;
+        const oldestAge = oldestDeathYear - oldest.yearOfBirth;
+
+        return age > oldestAge ? person : oldest;
+    });
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;

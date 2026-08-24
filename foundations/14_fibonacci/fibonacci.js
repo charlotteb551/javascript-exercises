c8aef3d02a290a9b5ce22ea7b6a3c5e87feb03bc
charlotteb551 <295230572+charlotteb551@users.noreply.github.com>
2026-08-24 11:46:02 +0100

    Task 14 Complete

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    let count
+    if (typeof position !== 'number') {
+        count = parseInt(position)
+    } else {
+        count = position
+    }
 
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (let i = 2; i <= count; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return firstPrev;
 };
 
 // Do not edit below this line
